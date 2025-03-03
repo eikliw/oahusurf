cf50d20af064ed646385eda91092c6d3319b1ed2
eikliw <eikliw@gmail.com>
2025-03-03 18:39:07 -0500

    2.9

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove the export output for now as it's not compatible with Sanity Studio integration
-  // output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['images.unsplash.com', 'cdn.sanity.io'],
     unoptimized: true,
   },
+  output: 'export',
+  distDir: 'out',
   // Add transpilePackages for Sanity Studio
   transpilePackages: ['sanity'],
 };
